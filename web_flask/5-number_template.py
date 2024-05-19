3c0fac4a03cceb268493a93a272c2b96e8191879
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2024-05-19 19:17:42 +0100

    auto pep8

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -38,7 +38,8 @@ def number(n):
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    """Returns a message when routing /number_template/<n> only if n is an integer"""
+    """Returns a message when routing
+    /number_template/<n> only if n is an integer"""
     return render_template("5-number.html", n=n)
 
 
